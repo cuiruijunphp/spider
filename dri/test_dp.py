9f5764ca00fe606b82a3cc761919254ac511f9c5
cuiruijunphp <cuiruijuphp@163.com>
2025-06-23 21:16:03 +0800

    test dissionage

--- dri/test_dp.py
+++ dri/test_dp.py
@@ -3,6 +3,8 @@ import os, random, platform
 
 co = ChromiumOptions()
 if platform.system().lower() == 'linux':
+    import random
+    
     user_dir = f'/tmp/dp_user_{os.getpid()}_{random.randint(1000,9999)}'
     port = random.randint(30000, 40000)
     co.set_argument('--no-sandbox')
